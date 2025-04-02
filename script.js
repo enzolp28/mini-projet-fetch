50c6a8a7f7ec0d38e8148412bf2d5e63b7d24b03
enzolp28 <laposta.enzo@gmail.com>
2025-04-02 13:02:09 +0200

    input search user

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('#load-users')
 const divList = document.querySelector('.user-list')
+const searchInput = document.querySelector('#search-input')
 
 async function displayUsers() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
@@ -7,10 +8,13 @@ async function displayUsers() {
     return user
 }
 
+let allUsers = []
+
 button.addEventListener('click', async () => {
     divList.innerHTML = ""
     try {
         const users = await displayUsers()
+        allUsers = await displayUsers()
         users.forEach(user => {
             const list = document.createElement('p')
             list.textContent = `${user.name} - ${user.email}`
@@ -22,4 +26,23 @@ button.addEventListener('click', async () => {
     } catch (error) {
 
     }
+})
+
+
+
+searchInput.addEventListener('input', () => {
+
+    const searchValue = searchInput.value.toLowerCase()
+    const resultSearch = allUsers.filter(user => user.name.toLowerCase().includes(searchValue))
+    console.log(resultSearch);
+    divList.innerHTML = ""
+
+    resultSearch.forEach(result => {
+        const list = document.createElement('p')
+        list.textContent = result.name
+
+        divList.appendChild(list)
+    })
+
+
 })
